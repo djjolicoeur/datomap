766d26026a87f6a3294819293a4330c4079b461a
djjolicoeur <djjolicoeur@gmail.com>
2017-08-12 20:42:32 -0400

    Add plugin code

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject datomap "0.1.0-SNAPSHOT"
+(defproject djjolicoeur/datomap "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[aysylu/loom "1.0.0"]
-                 [org.clojure/clojure "1.8.0"]
                  [hiccup "1.0.5"]
                  [jcf/dorothy "0.0.7-SNAPSHOT"]]
   :profiles {:dev {:plugins [[lein-kibit "0.1.2"]]
@@ -13,4 +12,5 @@
                    :dependencies [[com.stuartsierra/component "0.2.3"]
                                   [reloaded.repl "0.2.1"]]}
              :provided {:dependencies [[org.clojure/clojure "1.8.0"]
-                                       [com.datomic/datomic-free "0.9.5404"]]}})
+                                       [com.datomic/datomic-free "0.9.5404"]]}}
+  :eval-in-leiningen true)
