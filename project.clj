3e87a63d981d9690d2aa3f43814e1aca4f56ec10
Daniel Jolicoeur <djjolicoeur@gmail.com>
2017-10-13 21:21:24 -0400

    Remove plugin stuff from project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject djjolicoeur/datomap "0.1.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Datomic schema utilities and visualization tools"
+  :url "http://github.com/djjolicoeur/datomap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[aysylu/loom "1.0.0"]
@@ -11,6 +11,4 @@
                    :dependencies [[com.stuartsierra/component "0.2.3"]
                                   [reloaded.repl "0.2.1"]
                                   [org.clojure/clojure "1.8.0"]
-                                  [com.datomic/datomic-free "0.9.5404"]]}}
-  :jvm-opts ["-Xmx1g"]
-  :eval-in-leiningen true)
+                                  [com.datomic/datomic-free "0.9.5404"]]}})
